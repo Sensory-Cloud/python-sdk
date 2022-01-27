e5251989d2f2e62c589e875e1fb39db48a197337
Jonathan Hersch <jhersch@sensoryinc.com>
2022-01-27 09:32:53 -0700

    updated setup.py to use pypi readme for long description and bumped the version number

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages, setup
 with open("requirements.txt", "r") as requirements_txt:
     requirements = requirements_txt.read().splitlines()
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("PyPI-README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="sensory_cloud",
-    version="0.6.0",
+    version="0.6.1",
     description="Python SDK for Sensory Cloud",
     author="Jonathan Hersch",
     author_email="jhersch@sensoryinc.com",
